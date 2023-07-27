5bdf14911d4d282c2e4e82ab329fbaea9eecf7e3
pbl.gllgs <pbl.gllgs@gmail.com>
2023-07-27 11:48:48 -0400

    initial commit

---
+++ src/test/java/com/pblgllgs/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pblgllgs.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
