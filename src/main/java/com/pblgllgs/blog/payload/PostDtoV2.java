f5d7797d551674da96dc12a20283fe2a5183fc32
pbl.gllgs <pbl.gllgs@gmail.com>
2023-07-28 22:24:22 -0400

    dto for versioning

---
+++ src/main/java/com/pblgllgs/blog/payload/PostDtoV2.java
@@ -0,0 +1,26 @@
+package com.pblgllgs.blog.payload;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
+import java.util.Set;
+
+@Data
+public class PostDtoV2 {
+
+    private Long id;
+    @NotBlank
+    @Size(min = 2, message = "Post title should have al least 2 characters")
+    private String title;
+    @NotBlank
+    @Size(min = 10, message = "Post description should have al least 10 characters")
+    private String description;
+    @NotBlank
+    @Size(min = 10, message = "Post content should have al least 100 characters")
+    private String content;
+    private Set<CommentDto> comments;
+
+    private List<String> tags;
+}
